bd4f95fcc10a38942b5e49e323750760bdad79f8
Kasper4649 <imzxz123456@gmail.com>
2021-11-27 23:14:37 +0800

    feat: able to notify if course available

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/launcher"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const (
+	requestUrl  string = "http://yjs.ustc.edu.cn/course/query.asp?mode=dept"
+	notifyToken string = ""
+)
+
+var courseNumbers = []string{"EIEN6001U", "PHIL6301U"}
+
+func main() {
+	u := launcher.New().NoSandbox(true).MustLaunch()
+	page := rod.New().ControlURL(u).MustConnect().MustIncognito().MustPage(requestUrl).MustWindowFullscreen()
+	frame := page.MustWaitLoad().MustElement("#query").MustFrame()
+	frame.MustElement("#year1").MustSelect("2021-2022")
+	frame.MustElement("#radio1").MustClick()
+	frame.MustElement("#kkdept").MustSelect("A14-软件学院苏州")
+	frame.MustElementX("//input[@type='submit']").MustClick()
+	time.Sleep(time.Second * 2)
+
+	for _, cNo := range courseNumbers {
+		courses := frame.MustElementsX("//tr[@class='bt06' and contains(., '" + cNo + "')]")
+		for _, course := range courses {
+			texts := strings.Split(course.MustText(), "\t")
+			selectNum, _ := strconv.Atoi(texts[len(texts)-3])
+			fullNum, _ := strconv.Atoi(texts[len(texts)-2])
+			if selectNum < fullNum {
+				text := fmt.Sprintf("%s的%s有%d空位。", texts[2], texts[1], fullNum-selectNum)
+				notify(text)
+			}
+		}
+	}
+}
+
+func notify(text string) {
+	notifyUrl := fmt.Sprintf("https://api.day.app/%s/选课空位通知/%s", notifyToken, url.QueryEscape(text))
+	_, _ = http.Get(notifyUrl)
+}
