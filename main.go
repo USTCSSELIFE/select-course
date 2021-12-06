fbf51aa9d53a28a2bed0d35cc56913e0bc1941ee
Kasper4649 <imzxz123456@gmail.com>
2021-12-06 16:20:49 +0800

    perf(use MustElement() instead of splitting text):

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"net/url"
 	"strconv"
-	"strings"
 	"time"
 )
 
@@ -16,7 +15,7 @@ const (
 	notifyToken string = ""
 )
 
-var courseNumbers = []string{"EIEN6001U", "PHIL6301U"}
+var courseNumbers = []string{"EIEN6017P"}
 
 func main() {
 	u := launcher.New().NoSandbox(true).MustLaunch()
@@ -25,17 +24,20 @@ func main() {
 	frame.MustElement("#year1").MustSelect("2021-2022")
 	frame.MustElement("#radio1").MustClick()
 	frame.MustElement("#kkdept").MustSelect("A14-软件学院苏州")
-	frame.MustElementX("//input[@type='submit']").MustClick()
+	frame.MustElementX(`//input[@type="submit"]`).MustClick()
 	time.Sleep(time.Second * 2)
 
 	for _, cNo := range courseNumbers {
-		courses := frame.MustElementsX("//tr[@class='bt06' and contains(., '" + cNo + "')]")
+		courses := frame.MustElementsX(`//tr[@class="bt06" and contains(., "` + cNo + `")]`)
 		for _, course := range courses {
-			texts := strings.Split(course.MustText(), "\t")
-			selectNum, _ := strconv.Atoi(texts[len(texts)-3])
-			fullNum, _ := strconv.Atoi(texts[len(texts)-2])
+			courseName := course.MustElementsX(`.//a[@title="详细信息"]`)[0].MustText()
+			selectNumStr := course.MustElementsX(`.//a[@title="详细信息"]`)[1].MustText()
+			fullNumStr := course.MustElementsX(`.//td[@class="bt06"]`)[6].MustText()
+			courseTeacher := course.MustElementsX(`.//td[@class="bt06"]`)[2].MustText()
+			selectNum, _ := strconv.Atoi(selectNumStr)
+			fullNum, _ := strconv.Atoi(fullNumStr)
 			if selectNum < fullNum {
-				text := fmt.Sprintf("%s的%s有%d空位。", texts[2], texts[1], fullNum-selectNum)
+				text := fmt.Sprintf("%s的%s有%d空位。", courseTeacher, courseName, fullNum-selectNum)
 				notify(text)
 			}
 		}
